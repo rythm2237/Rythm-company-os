4bfcb31692f3b9c5a27af7f2f418c4b3b4c67ed6
Yaser Tayyebialashti <95166759+rythm2237@users.noreply.github.com>
2026-09-03 07:43:37 +0200

    Add verified organization profiles to structured data

--- components/brand/OrganizationStructuredData.tsx
+++ components/brand/OrganizationStructuredData.tsx
@@ -1,10 +1,24 @@
 import { ORGANIZATION_GRAPH } from "@/lib/seo/site";
 
+const VERIFIED_ORGANIZATION_PROFILES = [
+  "https://www.linkedin.com/company/rythm-company-os",
+  "https://github.com/Rythm-os",
+] as const;
+
+const organizationGraphWithVerifiedProfiles = {
+  ...ORGANIZATION_GRAPH,
+  "@graph": ORGANIZATION_GRAPH["@graph"].map((entity) =>
+    entity["@type"] === "Organization"
+      ? { ...entity, sameAs: VERIFIED_ORGANIZATION_PROFILES }
+      : entity,
+  ),
+};
+
 export default function OrganizationStructuredData() {
   return (
     <script
       dangerouslySetInnerHTML={{
-        __html: JSON.stringify(ORGANIZATION_GRAPH).replace(/</g, "\\u003c"),
+        __html: JSON.stringify(organizationGraphWithVerifiedProfiles).replace(/</g, "\\u003c"),
       }}
       type="application/ld+json"
     />
